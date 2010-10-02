630f7906a6a39bfc33bcafcf3283f01b55d5903c
Alberto De Bortoli <devnull@localhost>
2010-10-03 01:11:03 +0200

    Minor fix

--- SpeakTweet/views/timeline/SelectedCellBackgroundView.m
+++ SpeakTweet/views/timeline/SelectedCellBackgroundView.m
@@ -14,23 +14,13 @@
 	return self;
 }
 
-- (void)drawRect:(CGRect)rect {
-/*	CGRect r = CGRectMake(0, 12, 320, rect.size.height-24);
-	[[UIImage imageNamed:@"timelineBackground_01.png"] drawAtPoint:CGPointMake(0,0)];
-	[[UIImage imageNamed:@"timelineBackground_02.png"] drawAsPatternInRect:r];
-	[[UIImage imageNamed:@"timelineBackground_03.png"] drawAtPoint:CGPointMake(0,rect.size.height-12)];
-*/
-	
+- (void)drawRect:(CGRect)rect {	
 	CGContextRef context = UIGraphicsGetCurrentContext();
 	CGContextDrawLinearGradient(context, [[Colors instance] timelineSelectedBackgroundGradient], 
 								CGPointMake(0,0), CGPointMake(0,rect.size.height), 
 								kCGGradientDrawsBeforeStartLocation|kCGGradientDrawsAfterEndLocation);
 
-//	CGContextRef context = UIGraphicsGetCurrentContext();
-//	CGSize shadowPosition = {0,-1};
-//	CGContextSetShadowWithColor(context, shadowPosition, 2, [UIColor whiteColor].CGColor);
 	[StatusCell drawTexts:status selected:YES];
-//	CGContextSetShadowWithColor(context, shadowPosition, 1, NULL);
 }
 
 - (void)dealloc {
